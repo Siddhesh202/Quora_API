6f5a9162191ce313fe651c3a711dc6de726d74ae
Siddhesh Pisal <siddheshpisal76@gmail.com>
2024-04-17 00:07:25 +0530

    Added user and question model

---
+++ src/main/java/com/example/quora/models/Question.java
@@ -0,0 +1,40 @@
+package com.example.quora.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date created_at;
+
+    @ManyToOne
+    private User user;
+
+}
---
+++ src/main/java/com/example/quora/models/User.java
@@ -0,0 +1,32 @@
+package com.example.quora.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String bio;
+
+    @OneToMany(mappedBy = "user")
+    private List<Question> questions = new ArrayList<>();
+}
